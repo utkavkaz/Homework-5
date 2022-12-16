6386b553ca502c9258bee00d6738dedea670acd4
utkavaz <ntkavkaz@rambler.ru>
2022-12-16 20:27:32 +0300

    homework

---
+++ Z_1_HARD/Program.cs
@@ -0,0 +1,89 @@
+﻿// задача 1 HARD необязательная. Сгенерировать массив случайных целых чисел 
+// размерностью m*n (размерность вводим с клавиатуры). 
+// Вывести на экран красивенько таблицей. 
+// Найти минимальное число и его индекс, найти максимальное число и его индекс. 
+// Вывести эту информацию на экран.
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(100);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j],2}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Max(int[,] array)
+{
+    int max = array[0, 0];
+    int index1 = 0;
+    int index2 = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] > max)
+            {
+                max = array[i, j];
+                index1 = i;
+                index2 = j;
+            }
+        }
+    }
+    Console.WriteLine($"Максимальный элемент массива = {max} с индексом [{index1},{index2}]");
+}
+
+
+void Min(int[,] array)
+{
+    int min = array[0, 0];
+    int index3 = 0;
+    int index4 = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < min)
+            {
+                min = array[i, j];
+                index3 = i;
+                index4 = j;
+            }
+        }
+    }
+    Console.WriteLine($"Минимальный элемент массива = {min} с индексом [{index3},{index4}]");
+}
+
+Console.Write("Введите количество строк массива: ");
+int heighty = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int width = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+int[,] myArray = new int[heighty, width];
+
+FillArray(myArray);
+PrintArray(myArray);
+Console.WriteLine();
+
+Max(myArray);
+Console.WriteLine();
+
+Min(myArray);
---
+++ Z_41/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("Введите число: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        array[i] = num;
+    }
+}
+
+int Count(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count += 1;
+    }
+    return count;
+}
+
+Console.Write("Введите количество чисел: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] Array = new int[size];
+
+FillArray(Array);
+Console.WriteLine("Количество чисел больше 0 равно " + Count(Array));
